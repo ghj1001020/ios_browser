0ae8882a3150a3995c44464d7cb2648a072f720b
ghj1001020 <33592073+ghj1001020@users.noreply.github.com>
2021-05-19 20:24:18 +0900

    로그유틸

--- Browser/Common/Util/Log.swift
+++ Browser/Common/Util/Log.swift
@@ -10,18 +10,30 @@ import Foundation
 
 class Log {
     
+    private static let MAX_MSG = 1500
+    
+    
     static func p(_ _message : String? , _file: String=#file, _function: String=#function, _line: Int=#line, _column: Int=#column )
     {
         if( AppConfig.LOG_BLOCK ) {
             return
         }
         
+        // 헤더
         var fileName = ""
         if let index = _file.lastIndex(of: "/") {
             fileName = String(_file[_file.index(index, offsetBy: 1)..<_file.endIndex])
         }
+        let head : String = "[\(fileName) > \(_function)() Line:\(_line)] >> "
+
         let message = _message.nullToString(defaultValue: "")
-        
-        print( "[\(fileName) , \(_function) , Line:\(_line)] >> \(message)" )
+        // 글자수 제한
+        let length : Int = message.count/MAX_MSG + 1
+        for idx in 0..<length {
+            // 바디
+            let lastIdx = (idx+1)*MAX_MSG > message.count ? message.count : (idx+1)*MAX_MSG
+            let body = message.substring(from: idx*MAX_MSG, to: lastIdx)
+            print( "\(head)\(body)" )
+        }
     }
 }
--- Browser/Common/Util/StringExt.swift
+++ Browser/Common/Util/StringExt.swift
@@ -24,3 +24,17 @@ extension Optional where Wrapped == String {
         return value
     }
 }
+
+extension String {
+    subscript( bounds: CountableClosedRange<Int> ) -> String {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return String(self[start...end])
+    }
+    
+    func substring(from: Int, to: Int) -> String {
+        let start = self.index(self.startIndex, offsetBy: from)
+        let end = self.index(start, offsetBy: to-from)
+        return String(self[start..<end])
+    }
+}
